8833d230fc807475aa5620c04243b916f8443fbd
jiricodes <novotny.049@gmail.com>
2023-08-02 10:55:25 +0300

    check that words are unique

--- scripts/checkdict.py
+++ scripts/checkdict.py
@@ -2,11 +2,15 @@ import sys
 
 if len(sys.argv) < 2:
     print("missing dict argument")
-    exit
+    exit(1)
 
-f = open(sys.argv[1], "r")
-for line in f.readlines():
-    line = line.strip()
-    if len(line) != 5 or not line.isalpha():
-        print(f"error at {line}")
-        exit
+with open(sys.argv[1], "r") as f:
+    for line in f.readlines():
+        line = line.strip()
+        if len(line) != 5 or not line.isalpha():
+            print(f"error at {line}")
+            exit(1)
+with open(sys.argv[1], "r") as f:
+    words = f.readlines();
+    set_words = set(words);
+    print(f"words == set: {len(words) == len(set_words)}")
