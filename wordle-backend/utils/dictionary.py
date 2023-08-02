c2d8be9ca9febab4609ea554b007a1c92ed994d7
jiricodes <novotny.049@gmail.com>
2023-08-02 14:37:01 +0300

    movin errors to make simpler structure

--- wordle-backend/utils/dictionary.py
+++ wordle-backend/utils/dictionary.py
@@ -1,4 +1,8 @@
-from error import InvalidDictionary
+class InvalidDictionary(Exception):
+
+    def __init__(self, message) -> None:
+        self.message = message
+        super().__init__(self.message)
 
 def validate_dictionary(words):
     set_words = set(words)
@@ -7,9 +11,9 @@ def validate_dictionary(words):
     pass
 
 def load_dictionary(filename):
-with open(sys.argv[1], "r") as f:
-    for line in f.readlines():
-        line = line.strip()
-        if len(line) != 5 or not line.isalpha():
-            print(f"error at {line}")
-            exit(1)
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if len(line) != 5 or not line.isalpha():
+                print(f"error at {line}")
+                exit(1)
