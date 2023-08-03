c9f95ca3b85f9e1deff71d298dde26c85c4092b1
Joonatan Aurasmaa <jaurasma@c1r3p6.hive.fi>
2023-08-03 16:51:21 +0300

    no keyboard, mouse works

--- wordle-backend/static/js/script.js
+++ wordle-backend/static/js/script.js
@@ -1,54 +1,42 @@
 const gridContainer = document.querySelector('.grid-container');
 let selectedBox = null;
-var isItValid;
 
 const boxElements = document.querySelectorAll('.box');
 const rowContainers = document.querySelectorAll('.grid-row');
-let keyboardButtons;
 
 async function sendRowData(rowContainer) {
-    const boxElementsInRow = rowContainer.querySelectorAll('.box');
-    const rowData = [];
+	const boxElementsInRow = rowContainer.querySelectorAll('.box');
+	const rowData = [];
 
-    boxElementsInRow.forEach((box) => {
-        const input = box.querySelector('input');
-        const value = input.value.toUpperCase();
-        rowData.push(value);
-    });
+	boxElementsInRow.forEach((box) => {
+		const input = box.querySelector('input');
+		const value = input.value.toUpperCase();
+		rowData.push(value);
+	});
 
-    // Wait for the response from the server using await
-    const validRow = await sendRowDataToServer(rowData, rowContainer);
-    console.log("validRow is: " + validRow);
-
-    return validRow;
+	// Wait for the response from the server using await
+	const validRow = await sendRowDataToServer(rowData, rowContainer);
+	return validRow;
 }
 
 async function sendRowDataToServer(rowData, rowContainer) {
-    try {
-        const response = await fetch('/api/v1/guess', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ guess: rowData }),
-        });
-
-        const data = await response.json();
-        console.log(data);
-        updateBoxes(rowContainer, data.valid, data.state, data.stage);
-        return data.valid;
-    } catch (error) {
-        console.error('Error:', error);
-        return false;
-    }
+	try {
+		const response = await fetch('/api/v1/guess', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ guess: rowData }),
+		});
+		const data = await response.json();
+		updateBoxes(rowContainer, data.valid, data.state, data.stage);
+		return data.valid;
+	} catch (error) {
+		console.error('Error:', error);
+		return false;
+	}
 }
 
-// Rest of the code remains the same
-
-function focusOnGameBoard() {
-	gridContainer.focus();
-  }
-// Starting the game
 function focusOnFirstBox() {
 	const firstBox = boxElements[0];
 	selectedBox = firstBox;
@@ -57,97 +45,50 @@ function focusOnFirstBox() {
 }
 
 function updateBoxes(rowContainer, isValid, stringsFromPython, stage) {
-    // Get all the boxes in the row
-    const boxElementsInRow = rowContainer.querySelectorAll('.box');
+	// Get all the boxes in the row
+	const boxElementsInRow = rowContainer.querySelectorAll('.box');
 	console.log(boxElementsInRow);
-    // Loop through the box elements and apply the colors based on the numbers received
-    if (isValid) {
+	// Loop through the box elements and apply the colors based on the numbers received
+	if (isValid) {
 		const errorMessage = document.getElementById('error-message');
-        errorMessage.style.display = 'none';
-        // Loop through the box elements and apply the colors based on the numbers received
-        boxElementsInRow.forEach((box, index) => {
-            const string = stringsFromPython[index];
-            if (string === 'yellow') {
-                box.style.backgroundColor = 'rgb(177, 159, 77)';
-            } else if (string === 'green') {
-                box.style.backgroundColor = 'rgb(97, 139, 85)';
-            } else if (string === 'gray') {
-                box.style.backgroundColor = 'dimgray';
-            }
-        });
-    } else {
-        // Row is not valid, you can show an error message or take appropriate action
+		errorMessage.style.display = 'none';
+		// Loop through the box elements and apply the colors based on the numbers received
+		boxElementsInRow.forEach((box, index) => {
+			const string = stringsFromPython[index];
+			if (string === 'yellow') {
+				box.style.backgroundColor = 'rgb(177, 159, 77)';
+			} else if (string === 'green') {
+				box.style.backgroundColor = 'rgb(97, 139, 85)';
+			} else if (string === 'gray') {
+				box.style.backgroundColor = 'dimgray';
+			}
+		});
+	} else {
+		// Row is not valid, you can show an error message or take appropriate action
 		const errorMessage = document.getElementById('error-message');
-        errorMessage.style.display = 'block';
-        console.log('Row is not valid. You need to modify the current row.');
-    }
+		errorMessage.style.display = 'block';
+		console.log('Row is not valid. You need to modify the current row.');
+	}
 	if (stage == "win") {
 		const winMessage = document.getElementById('win-message');
-        winMessage.style.display = 'block';
-        console.log('Game won');
+		winMessage.style.display = 'block';
+		console.log('Game won');
 	}
 	else if (stage == "loss") {
 		const lossMessage = document.getElementById('loss-message');
-        lossMessage.style.display = 'block';
-        console.log('Game lost');
+		lossMessage.style.display = 'block';
+		console.log('Game lost');
 	}
 	else {
 		const winMessage = document.getElementById('win-message');
 		const lossMessage = document.getElementById('loss-message');
-        winMessage.style.display = 'none';
-        lossMessage.style.display = 'none';
+		winMessage.style.display = 'none';
+		lossMessage.style.display = 'none';
 	}
 }
 document.addEventListener('DOMContentLoaded', () => {
 	focusOnFirstBox(); // Call this function to set the focus on the first box
-	
-	// Add event listeners to the keyboard buttons
-	keyboardButtons = document.querySelectorAll('.keyboard button');
-	keyboardButtons.forEach((button) => {
-		button.addEventListener('click', () => {
-		  const letter = button.getAttribute('data-letter');
-		  if (selectedBox) {
-			const input = selectedBox.querySelector('input');
-			input.value = letter.toUpperCase();
-			input.previousElementSibling.textContent = letter.toUpperCase();
-	
-			const currentIndex = Array.from(boxElements).indexOf(selectedBox);
-			const nextIndex = currentIndex + 1;
-	
-			if (nextIndex < boxElements.length && !isRowFilled(selectedBox.closest('.grid-row'))) {
-			  selectedBox.classList.remove('selected-box');
-			  selectedBox = boxElements[nextIndex];
-			  selectedBox.classList.add('selected-box');
-			  selectedBox.querySelector('input').focus();
-			}
-		  }
-	
-		  // After handling web keyboard input, focus back on the game board
-		  button.blur();
-		  focusOnGameBoard();
-		  // Blur the web keyboard button to return focus to the game board
-		});
-	  });
-	
-	// Add event listener to the delete button
-	const deleteButton = document.getElementById('delete-button');
-	deleteButton.addEventListener('click', () => {
-		const input = selectedBox.querySelector('input');
-		input.value = '';
-		input.previousElementSibling.textContent = '';
-		
-		const currentIndex = Array.from(boxElements).indexOf(selectedBox);
-		const previousIndex = currentIndex - 1;
-		
-		if (previousIndex >= 0 && currentIndex % 5 !== 0) {
-			selectedBox.classList.remove('selected-box');
-			selectedBox = boxElements[previousIndex];
-			selectedBox.classList.add('selected-box');
-			selectedBox.querySelector('input').focus();
-		}
-	});
 });
-
 
 function isRowFilled(rowContainer) {
 	const boxElementsInRow = rowContainer.querySelectorAll('.box');
@@ -165,7 +106,6 @@ async function moveToNextRow(currentRowContainer) {
 	let validRow;
 	if (nextRowContainer) {
 		validRow = await sendRowData(currentRowContainer);
-		console.log("now we are cheking if row is valid " + validRow);
 		if (validRow == true) {
 		selectedBox.classList.remove('selected-box');
 		selectedBox = nextRowContainer.querySelector('.box');
@@ -185,38 +125,6 @@ async function moveToNextRowIfFilled(currentRowContainer) {
 		await moveToNextRow(currentRowContainer);
 	}
 }
-
-// Click event for each box and first box in each row using event delegation
-function handleMouseOut(box) {
-  if (box !== selectedBox) {
-	box.classList.remove('hover');
-  }
-}
-function handleMouseOver(box) {
-	if (box !== selectedBox) {
-	  box.classList.add('hover');
-	}
-  }
-  
-gridContainer.addEventListener('click', (e) => {
-	const target = e.target;
-	if (target.classList.contains('box')) {
-		handleBoxClick(target);
-	} else if (target.classList.contains('first-box')) {
-		handleFirstBoxClick(target);
-	}
-});
-
-// Mouseover and Mouseout events for each box
-boxElements.forEach((box) => {
-  box.addEventListener('mouseover', () => {
-	handleMouseOver(box);
-  });
-
-  box.addEventListener('mouseout', () => {
-	handleMouseOut(box);
-  });
-});
 
 gridContainer.addEventListener('keydown', (e) => {
 	if (e.key === 'Backspace') {
@@ -264,4 +172,17 @@ gridContainer.addEventListener('keydown', (e) => {
 		moveToNextRowIfFilled(currentRowContainer);
 		e.preventDefault(); // Prevent the default behavior of "Enter" (like submitting a form)
 	}
+});
+
+function focusOnGameBoard() {
+	gridContainer.focus();
+}
+
+document.addEventListener('pointerdown', (e) => {
+	if (!gridContainer.contains(e.target)) {
+		// Pointerdown event originated outside the grid, do not change focus
+		e.preventDefault(); // Prevent losing focus from the game board
+		return;
+	}
+	focusOnGameBoard();
 });
